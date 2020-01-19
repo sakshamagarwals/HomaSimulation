7a23d9564b20c4fc7e0374d195f13c32292cfaf4
qizhe <qizhe.cai@gmail.com>
2020-01-18 20:29:44 -0500

    add config for large topology

---
+++ RpcTransportDesign/OMNeT++Simulation/config/fbb/tor_64_256/config.py
@@ -0,0 +1,29 @@
+template_host = """    <hostConfig id=\"{}\">
+        <transportConfig>
+        </transportConfig>
+        <appConfig>
+            <isSender>true</isSender>
+            <destIds>-1</destIds>
+        </appConfig>
+    </hostConfig>"""
+
+template_config = """<interface hosts='**.host[{}..{}]' address='10.x.{}.x' netmask='255.x.x.x'/>
+"""
+template_head = """<topologyConfig>
+    <param>
+    </param>
+
+    <IPv4Configurator> 
+    {}
+    </IPv4Configurator>
+    {}
+</topologyConfig>"""
+
+config1 = ""
+config2 = ""
+for i in range(4):
+	config1 += template_config.format(i * 64, i * 64 + 63, i) + "\n"
+for i in range(64 * 4):
+	config2 += template_host.format(i) + "\n"
+
+print template_head.format(config1, config2)
